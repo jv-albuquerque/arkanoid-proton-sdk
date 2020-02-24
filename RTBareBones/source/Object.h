00162748965b9bb09f12d227d1696b9f6b9b796d
jvalbuquerque <jv.albuquerque@live.com>
2020-02-24 14:37:46 -0300

    Transform the class to a pure abstract

--- RTBareBones/source/Object.h
+++ RTBareBones/source/Object.h
@@ -1,16 +1,11 @@
 #pragma once
+
+#include "PlatformPrecomp.h"
+
 class Object
 {
-protected:
-	CL_Vec2f pos;
-	CL_Vec4f color;
-	CL_Vec2f size;
-	float speed;
-
-	uint32 makeRGBA(CL_Vec4f _color);
-
 public:
-	virtual void Init(CL_Vec2f _initialPos, CL_Vec2f _size, CL_Vec4f _color, float _speed = 0) = 0;
+	virtual void Init(CL_Vec2f _initialPos, CL_Vec2f _size, uint32 _color, float _speed = 0) = 0;
 	virtual void Draw() = 0;
 	virtual void Update(float deltaTick) = 0;
 	virtual void OnArcadeInput(VariantList* pVList) = 0;
